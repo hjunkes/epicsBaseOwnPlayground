59d565db61c262c754ef3fe6cbb86ac722fb90ce
Heinz Junkes <junkes@fhi-berlin.mpg.de>
2019-03-07 16:35:35 +0100

    Next try

--- .ci/travis-build.sh
+++ .ci/travis-build.sh
@@ -54,7 +54,7 @@ then
   curl -L "https://github.com/mdavidsaver/rsb/releases/download/20171203-${RTEMS}/i386-rtems${RTEMS}-trusty-20171203-${RTEMS}.tar.bz2" \
   | tar -C / -xmj
 
-  sed -i "" -e '/^RTEMS_VERSION/d' -e '/^RTEMS_SERIES/d' -e '/^RTEMS_BASE/d' configure/os/CONFIG_SITE.Common.RTEMS
+  sed -i -e '/^RTEMS_VERSION/d' -e '/^RTEMS_SERIES/d' -e '/^RTEMS_BASE/d' configure/os/CONFIG_SITE.Common.RTEMS
   cat << EOF >> configure/os/CONFIG_SITE.Common.RTEMS
 RTEMS_VERSION=$RTEMS
 RTEMS_SERIES="${RTEMS%.*}"
